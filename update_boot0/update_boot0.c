1189b610009879ddfd9b9d2d1ad135b0bb5b011e
Kamil Trzcinski <ayufan@ayufan.eu>
2017-04-30 12:42:16 +0200

    Import A64 changes from lichee/brandy/pack-tools of A64-BSP-3.0.zip Allwinner A64 Linux 3.10 BSP found at http://wiki.pine64.org/index.php/Pine_A64_Software_Release#Linux_BSP_3.0

--- update_boot0/update_boot0.c
+++ update_boot0/update_boot0.c
@@ -13,7 +13,7 @@
 #define  MAX_PATH             (260)
 
 
-__asm__(".symver memcpy ,memcpy@GLIBC_2.2.5");
+//__asm__(".symver memcpy ,memcpy@GLIBC_2.2.5");
 void *script_file_decode(char *script_name);
 int update_for_boot0(char *boot0_name, int storage_type);
 //------------------------------------------------------------------------------------------------------------
@@ -409,7 +409,7 @@ int update_sdcard_info(char  *buf)
 
 				return -1;
 			}
-			//support to init 8 line 
+			//support to init 8 line
 			if(8 == card_info->line_sel[i])
 			{
 				//获取DATA4
@@ -572,6 +572,12 @@ int update_for_boot0(char *boot0_name, int storage_type)
         {
             boot0_head->prvt_head.debug_mode = 1;
         }
+
+	if (!script_parser_fetch("target", "power_mode", value))
+	{
+		boot0_head->prvt_head.power_mode = value[0];
+	}
+
 	//取出数据进行修正,DRAM参数
 	if(script_parser_sunkey_all("dram_para", (void *)boot0_head->prvt_head.dram_para))
 	{
--- update_uboot_fdt/uboot_head.h
+++ update_uboot_fdt/uboot_head.h
@@ -40,22 +40,24 @@ struct spare_boot_ctrl_head
 struct spare_boot_data_head
 {
 	unsigned int                dram_para[32];
-	int							run_clock;				// Mhz
-	int							run_core_vol;			// mV
-	int							uart_port;              // UART���������
-	normal_gpio_cfg             uart_gpio[2];           // UART������(���Դ�ӡ��)GPIO��Ϣ
-	int							twi_port;               // TWI���������
-	normal_gpio_cfg             twi_gpio[2];            // TWI������GPIO��Ϣ�����ڿ���TWI
-	int		                    work_mode;              // ����ģʽ
-    int                         storage_type;           // �洢��������  0��nand   1��sdcard    2: spinor
-    normal_gpio_cfg             nand_gpio[32];          // nand GPIO��Ϣ
-    char						nand_spare_data[256];	// nand ������Ϣ
-    normal_gpio_cfg             sdcard_gpio[32];		// sdcard GPIO��Ϣ
-    char             			sdcard_spare_data[256];	// sdcard ������Ϣ
-    int                         secureos_exist;
-    int				            dtb_offset;			
-    int							reserved[4];			// ��������λ, 256bytes align
-
+	int                         run_clock;              // Mhz
+	int                         run_core_vol;           // mV
+	int                         uart_port;              // UART控制器编号
+	normal_gpio_cfg             uart_gpio[2];           // UART控制器(调试打印口)GPIO信息
+	int                         twi_port;               // TWI控制器编号
+	normal_gpio_cfg             twi_gpio[2];            // TWI控制器GPIO信息，用于控制TWI
+	int                         work_mode;              // 工作模式
+	int                         storage_type;           // 存储介质类型  0：nand   1：sdcard    2: spinor
+	normal_gpio_cfg             nand_gpio[32];          // nand GPIO信息
+	char                        nand_spare_data[256];	// nand 额外信息
+	normal_gpio_cfg             sdcard_gpio[32];		// sdcard GPIO信息
+	char                        sdcard_spare_data[256];	// sdcard 额外信息
+	int                         secureos_exist;
+	unsigned int                uboot_start_sector_in_mmc;  //use in OTA update
+	int                         dtb_offset;                 //device tree in uboot
+	int                         boot_package_size;          //boot package size, boot0 pass this value
+	unsigned int				dram_scan_size;				//dram 扫描空间大小
+	int                         reserved[1];                // 保留数据位, 256bytes align
 };
 
 struct spare_boot_head_t
